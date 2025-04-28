3e1cd986236c8f2a93302f9308f0bbaf1c560f0e
jonas <jonaspfeifer@drei.at>
2025-04-29 01:24:33 +0200

    1. added new player sprites 2. updated player movement 3. added smoothing plugin to get rid of jitter: [Issue](https://github.com/godotengine/godot/issues/17823)

--- scripts/player_movement.gd
+++ scripts/player_movement.gd
@@ -1,28 +1,25 @@
 extends CharacterBody2D
 
 @export var player_speed := 4*16; 
-@onready var sprite := $AnimatedSprite2D
+@export var sprite: AnimatedSprite2D
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
+	pass
+		
+func _physics_process(_delta: float) -> void: 
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-
 	velocity = direction * player_speed;
+	move_and_slide()
 	
-	if direction.length() == 0:
+	sprite.flip_h = direction.x < 0;
+	
+	if direction.is_equal_approx(Vector2.ZERO):
 		sprite.play("player_idle")
 	elif direction.x > 0:
-		sprite.play("player_walk_right")
+		sprite.play("player_walk_horizontal")
 	elif direction.x < 0:
-		sprite.play("player_walk_left")
+		sprite.play("player_walk_horizontal")
 	elif direction.y < 0:
 		sprite.play("player_walk_up")
 	elif direction.y > 0:
 		sprite.play("player_walk_down")
-
-	move_and_slide()
