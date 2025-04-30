dd9d43841f06d1e5a6cab8d96bb504c9d2f6aaae
jonas <jonaspfeifer@drei.at>
2025-04-30 02:15:37 +0200

    added grown weed

--- scripts/weed.gd
+++ scripts/weed.gd
@@ -1,8 +1,21 @@
 extends Node2D
 class_name Weed
 
-@onready var gridPositionComponent: GridPositionComponent = $GridPositionComponent
+@onready var weedSprite = preload("res://sprites/weed/weed.png")
+@onready var weedGrownSprite = preload("res://sprites/weed/weed_grown.png")
 
+@onready var gridPositionComponent: GridPositionComponent = $GridPositionComponent
+@export var sprite: Sprite2D
+
+@export var is_grown: bool = false:
+	set(value):
+		is_grown = value
+		if sprite:
+			if value:
+				sprite.texture = weedGrownSprite
+			else:
+				sprite.texture = weedSprite
+		
 @export var grid_position: Vector2i:
 	set(value):
 		grid_position = value
@@ -13,3 +26,8 @@ class_name Weed
 func _ready() -> void:
 	gridPositionComponent.grid_position = grid_position
 	gridPositionComponent.updateLocation()
+	
+	if is_grown:
+		sprite.texture = weedGrownSprite
+	else:
+		sprite.texture = weedSprite
--- scripts/weed_spawner.gd
+++ scripts/weed_spawner.gd
@@ -9,11 +9,13 @@ func _ready() -> void:
 	for i in 6:
 		var weed: Weed = weedScene.instantiate()
 		weed.grid_position = Vector2i(-3+i,-3)
+		weed.is_grown = randf() < 0.2
 		
 		weedParentNode.add_child.call_deferred(weed)
 		
 	for i in 6:
 		var weed = weedScene.instantiate()
 		weed.grid_position = Vector2i(-3+i,2)
+		weed.is_grown = randf() < 0.4
 		
 		weedParentNode.add_child.call_deferred(weed)
